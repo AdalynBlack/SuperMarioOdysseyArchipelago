30984e5dbf9bca01f1c51fff9cdda56193eaec25
Adalyn Black <tomblackaz@gmail.com>
2026-07-11 10:51:58 -0700

    feat: include version file in every build for easier debugging

--- Mod/emu.sh
+++ Mod/emu.sh
@@ -7,6 +7,7 @@ cp -r ./atmosphere/contents/0100000000010000/exefs/* ./SMOAP/exefs/
 cp -r ./atmosphere/exefs_patches/StarlightBase/* ./SMOAP/exefs/
 
 set VERSION $(git describe --tags --abbrev)
+echo $VERSION > ./SMOAP/SMOAP_VERSION
 
 rm SMO_Archipelago_*_Emu.zip
 zip -r SMO_Archipelago_{$VERSION}_Emu.zip ./SMOAP/
--- Mod/switch.sh
+++ Mod/switch.sh
@@ -3,6 +3,7 @@
 cd ./starlight_patch_100/
 
 set VERSION $(git describe --tags --abbrev)
+echo $VERSION > ./atmosphere/contents/0100000000010000/SMOAP_VERSION
 
 rm SMO_Archipelago_*_Switch.zip
 zip -r SMO_Archipelago_{$VERSION}_Switch.zip ./atmosphere/
