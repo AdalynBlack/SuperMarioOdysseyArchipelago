f42ec168eaefa70cf9bcd488a03cb03ed36fc049
Adalyn Black <tomblackaz@gmail.com>
2026-08-12 00:02:29 -0700

    decomp: WorldList->mWorldList

--- Mod/include/game/WorldList/WorldList.h
+++ Mod/include/game/WorldList/WorldList.h
@@ -1,10 +1,34 @@
 #pragma once
 
+#include "sead/container/seadPtrArray.h"
+#include "sead/prim/seadSafeString.hpp"
+
 class WorldList {
+    class StageDBEntry {
+        public:
+            sead::FixedSafeString<0x80> name;
+            sead::FixedSafeString<0x40> category;
+            int useScenarioNo;
+    };
+
+    class WorldListEntry {
+        public:
+            const char* mainStageName;
+            const char* worldDevelopName;
+            int worldScenarioNum;
+            int clearMainScenarioNo;
+            int afterEndingScenarioNo;
+            int moonRockScenarioNo;
+            int* mainQuestIndexes;
+            sead::PtrArray<StageDBEntry> stageList;
+    };
+
     public:
         const char *getWorldDevelopName(int) const;
         int tryFindWorldIndexByStageName(char const *stageName) const;
         int getMoonRockScenarioNo(int worldId) const;
         int findUseScenarioNo(const char *stageName) const;
         bool checkIsMainStage(char const *stageName) const;
+
+        sead::PtrArray<WorldListEntry> mWorldList;
 };
